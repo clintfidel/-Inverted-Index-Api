e28ccd25be3e1b3e90c01059e3b5d234e33d6b29
clintfidel <clintfidel@icloud.com>
2017-05-02 11:48:29 +0100

    modified src/app.js files for endpoint config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-const express = require('express'),
-  dotenv = require('dotenv'),
-  config = require('../config/config.js');
+import express from 'express'; // importing  express framework
+import dotenv from 'dotenv'; // importing dotenv library
+import bodyParser from 'body-parser'; // importing bodyParser module
+import config from '../config/config';// imporrting the config file
+import api from './routes';
 
+
+/**
+ * creating an instance of dotenv abd express
+ */
 dotenv.config();
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api', api);
 process.env.NODE_ENV = 'production';
 const port = config[process.env.NODE_ENV].PORT;
 
-app.get('/', (req, res) => {
-  res.send('welcome to my first api');
-});
+
+/**
+ *  creating end points: createIndex and searchIndex
+ */
 app.listen(port, (err) => {
   if (!err) {
     console.log(`listening to server @ port ${port}`);
