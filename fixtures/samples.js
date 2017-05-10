74284a97bdfc0b56de29de96c7543544cdab4ec9
clintfidel <clintfidel@icloud.com>
2017-05-10 14:48:22 +0100

    update : add samples files for testing InvertedIndex class

--- fixtures/samples.js
+++ fixtures/samples.js
@@ -1,12 +1,8 @@
-const sampleResult = { an: [0],
-  inquiry: [0],
-  this: [0, 1],
+const sampleResult = { validBook: { this: [0, 1],
   string: [0, 1],
   seeks: [0],
-  from: [1],
-  third: [1],
   is: [1],
-  a: [1]
+  a: [1] }
 };
 
 module.exports.sampleResult = sampleResult;
