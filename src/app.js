f6db2582d367d7fa2c1bb9e010bf5380ea5cc74c
clintfidel <clintfidel@icloud.com>
2017-05-11 14:45:31 +0100

    Refactor: app.js to add v0 to endpoints(createIndex and search Index

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express'; // importing  express framework
 import dotenv from 'dotenv'; // importing dotenv library
 import bodyParser from 'body-parser'; // importing bodyParser module
-import config from '../config/config';// imporrting the config file
-import api from './routes';
+import config from '../config/config';// importing the config file
+import routes from './routes';
 
 /**
  * creating an instance of dotenv and express
@@ -11,14 +11,25 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', api);
-process.env.NODE_ENV = 'production';
+app.use('/api/v0', routes);
 
-const port = config[process.env.NODE_ENV].PORT;
-app.listen(port, (err) => {
-  if (!err) {
-    console.log(`listening to server @ port ${port}`);
-  } else {
-    console.log(err);
+// app.use((err, req, res, next) => {
+//   res.status(200).send('Request could not be completed. Please try again');
+// });
+
+/* const port = () => {
+  if (process.env.NODE_ENV === 'DEV') {
+    return config.development.PORT;
+  } else if (process.env.NODE_ENV === 'TEST') {
+    return config.test.PORT;
   }
+  return config.production.PORT;
+};*/
+
+
+
+// routes(app);
+app.listen(process.env.PORT_PROD, () => {
+  console.log(`listening to server @ port ${process.env.PORT_PROD}`);
 });
+
